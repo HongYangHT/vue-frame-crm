581e907e9353e0283bc07cd9bc2264e6a7be32ca
shenzq595559 <s123s123>
2020-11-04 19:57:26 +0800

    飞书，营销管理界面，需要展示二级菜单

--- src/frame/frame.vue
+++ src/frame/frame.vue
@@ -381,7 +381,7 @@
                     </div>
                 </div>
             </div>
-            <div :class="[{'layout-main': showMenuHead !== '4'},{'hidenNavTop': showMenuHead === '5'},{'showMoreNav': showMenuHead === '4'}]">
+            <div :class="[{'layout-main': showMenuHead !== '4'},{'showMoreNav': (showMenuHead === '4' || showMenuHead === '5') }]">
                 <div class="layout-main--content">
                     <!--
                         showMenuHead: 4  EIP集成隐藏菜单 头部信息
@@ -450,7 +450,7 @@
                         </div>
                     </div>
                     <div class="crm-wrapper" style="padding: 0 10px 15px!important;" ref="musterMenuListId" v-if="mpType === '2' && translateMusterMenuList && translateMusterMenuList.length
-                            && (showMenuHead === '1' || showMenuHead === '4' || showMenuHead === '2' || showMenuHead === '3')">
+                            && (showMenuHead === '1' || showMenuHead === '4' || showMenuHead === '2' || showMenuHead === '3' || showMenuHead === '5')">
                         <div class="customer-tt-table-low"
                              v-if="mpType === '2' && translateMusterMenuList && translateMusterMenuList.length">
                             <ul class="menu_eip">
